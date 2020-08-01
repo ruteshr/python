462c7466c35f0ff457f0b3c182893011d88f7675
ruteshr <44744748+ruteshr@users.noreply.github.com>
2020-08-01 06:33:19 +0530

    Create right_angle_pattern.py

---
+++ right_angle_pattern.py
@@ -0,0 +1,15 @@
+#right angle pattern
+a=int(input("enter no. of rows"))
+for i in range(a):
+    for j in range (i+1):
+        print("*",end="")
+    print()
+'''
+OUTPUT:
+
+enter no. of rows 4
+*
+**
+***
+****
+'''
