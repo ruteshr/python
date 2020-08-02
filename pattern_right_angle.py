c2b947948099bc09494eb5f51fcba7cabdb28c05
ruteshr <44744748+ruteshr@users.noreply.github.com>
2020-08-02 12:09:18 +0530

    Update pattern_right_angle.py

--- pattern_right_angle.py
+++ pattern_right_angle.py
@@ -1,34 +1,31 @@
-#right angle pattern
-a=int(input("enter no. of rows"))
-for i in range(a):
-    for j in range (i+1):
-        print("*",end="")
-    print()
+n=5
+for i in range(n):
+  for j in range(i+1):
+    print("*",end="")
+  print()
 '''
 OUTPUT:
-
-enter no. of rows 4
 *
 **
 ***
 ****
+*****
 '''
-#--------------------------------------
-a=5
-for i in range(a):
+n=5
+for i in range(n):
   for j in range(i+1):
     print(j,end="")
   print()
 '''
-output:
+OUTPUT:
 0
 01
 012
 0123
 01234
 '''
-#--------------------------------------
-for i in range(a):
+n=5
+for i in range(n):
   for j in range(1,i+2):
     print(j,end="")
   print()
@@ -39,4 +36,30 @@ OUTPUT:
 123
 1234
 12345
+'''
+n=5
+for i in range(1,n+1):
+  for j in range(i):
+    print(i,end="")
+  print()
+'''
+OUTPUT:
+1
+22
+333
+4444
+55555
+'''
+n=5
+for i in range(n):
+  for j in range(i+1):
+    print(n-i,end="")
+  print()
+  '''
+OUTPUT:
+5
+44
+333
+2222
+11111
 '''
