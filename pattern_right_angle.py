6a3c43edd3b27aad832570e02cfbb2b3250404ed
ruteshr <44744748+ruteshr@users.noreply.github.com>
2020-08-02 11:36:24 +0530

    Update pattern_right_angle.py

--- pattern_right_angle.py
+++ pattern_right_angle.py
@@ -13,3 +13,30 @@ enter no. of rows 4
 ***
 ****
 '''
+#--------------------------------------
+a=5
+for i in range(a):
+  for j in range(i+1):
+    print(j,end="")
+  print()
+'''
+output:
+0
+01
+012
+0123
+01234
+'''
+#--------------------------------------
+for i in range(a):
+  for j in range(1,i+2):
+    print(j,end="")
+  print()
+'''
+OUTPUT:
+1
+12
+123
+1234
+12345
+'''
