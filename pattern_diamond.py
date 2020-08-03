652ec2e72e7fc2acf795041ce42af67c7f2079f4
ruteshr <44744748+ruteshr@users.noreply.github.com>
2020-08-03 11:39:56 +0530

    Create pattern_diamond.py

---
+++ pattern_diamond.py
@@ -0,0 +1,77 @@
+n=5
+for i in range(n):
+  for j in range(n-i-1):
+    print(end=" ")
+  for j in range(2*i+1):
+    print("*",end="")
+  print()
+for i in range(1,n):#for i in range(n)-->for 10 line
+  for j in range(i):
+    print(end=" ")
+  for j in range(2*n-(2*i+1)):
+    print("*",end="")
+  print()
+'''
+OUTPUT:
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+'''
+#============================================
+n=5
+for i in range(n):
+  for j in range(n-i-1):
+    print(end=" ")
+  for j in range(2*i+1):
+    print(j+1,end="")
+  print()
+for i in range(1,n):
+  for j in range(i):
+    print(end=" ")
+  for j in range(2*n-(2*i+1)):
+    print(j+1,end="")
+  print()
+'''
+OUTPUT:
+    1
+   123
+  12345
+ 1234567
+123456789
+ 1234567
+  12345
+   123
+    1
+'''
+#============================================
+n=5
+for i in range(n):
+  for j in range(n-i-1):
+    print(end=" ")
+  for j in range(2*i+1):
+    print(i+1,end="")
+  print()
+for i in range(1,n):
+  for j in range(i):
+    print(end=" ")
+  for j in range(2*n-(2*i+1)):
+    print(n-i,end="")
+  print()
+'''
+OUTPUT:
+    1
+   222
+  33333
+ 4444444
+555555555
+ 4444444
+  33333
+   222
+    1
+'''
