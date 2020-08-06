2e0412b3888b39cbb313fedfa54f6d2e61c45006
ruteshr <44744748+ruteshr@users.noreply.github.com>
2020-08-06 05:48:38 +0530

    Update pattern_hrev_right_angle.py

--- pattern_hrev_right_angle.py
+++ pattern_hrev_right_angle.py
@@ -93,3 +93,45 @@ for i in range(n):
  4321
 54321
   '''
+#=======================================
+n=5
+count=0
+for i in range(n):
+    for j in range(n-i-1):
+        print(end="\t")
+    for j in range(i+1):
+        count=count+1
+    temp=count
+    for j in range(i+1):
+        print(temp,end="\t")
+        temp-=1
+    print()
+'''
+OUTPUT:
+                                1
+                        3       2
+                6       5       4
+        10      9       8       7
+15      14      13      12      11
+
+'''
+#===========================================================
+n=5
+count=0
+for i in range(n):
+    for j in range(n-i-1):
+        print(end="\t")
+    for j in range(i+1):
+        count=count+1
+        print(count,end="\t")
+    print()
+'''
+OUTPUT:
+                                1
+                        2       3
+                4       5       6
+        7       8       9       10
+11      12      13      14      15
+
+'''
+#===========================================================
