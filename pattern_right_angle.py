2afeb1c03acec933c3e539bf647c7c700db0c201
ruteshr <44744748+ruteshr@users.noreply.github.com>
2020-08-06 05:50:02 +0530

    Update pattern_right_angle.py

--- pattern_right_angle.py
+++ pattern_right_angle.py
@@ -82,3 +82,34 @@ OUTPUT:
 4 5 6
 7 8 9 10
 '''
+#===========================================================
+n=5
+count=0
+for i in range(n):
+    if(i%2==0):
+        for j in range(i+1):
+            count+=1
+            if(i==j):
+                print(count,end="")
+            else:
+                print(count,end="*")
+    if(i%2!=0):
+        for j in range(i+1):
+            count+=1
+        temp=count
+        for j in range(i+1):
+            if(i==j):
+                print(temp,end="")
+                temp-=1
+            else:
+                print(temp,end="*")
+                temp-=1
+    print()
+'''
+OUTPUT:
+1
+3*2
+4*5*6
+10*9*8*7
+11*12*13*14*15
+'''
