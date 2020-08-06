9cbdc7f4cdd7cad273bd0fb6537592e953088be2
ruteshr <44744748+ruteshr@users.noreply.github.com>
2020-08-06 06:00:19 +0530

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -18,3 +18,36 @@ def computegcd(a,b):
   return a
 n1,n2=int(input("enter two number")),int(input())
 print(computegcd(n1,n2))
+
+#=============================================
+#gcd of three numbers
+def gcd(a,b):
+    if(b==0):
+        return a
+    return gcd(b,a%b)
+a,b,c=18,27,36
+print(gcd(a,gcd(b,c)))
+'''
+OUTPUT:
+9
+'''
+#=============================================
+#gcd for multiple/array of inputs
+def find_gcd(a,b):
+    if(b==0):
+        return a
+    return find_gcd(b,a%b)
+a=list(map(int,input().split(" ")))#getting i/p from the user and store it in the list
+n1=a[0]
+n2=a[1]
+gcd=find_gcd(n1,n2)
+for i in range(2,len(a)):
+    gcd=find_gcd(gcd,a[i])
+print(gcd)
+'''
+INPUT:
+    16 8 4 20 36 40 84
+
+OUTPUT:
+    4
+'''
